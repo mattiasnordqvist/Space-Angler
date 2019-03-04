3798a0286341b3b9c61057d9a0f4bf86ec5a09e9
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2019-03-04 17:45:07 +0100

    Fixes #1

--- SpaceAngler/Sql/filler.sql
+++ SpaceAngler/Sql/filler.sql
@@ -1,4 +1,9 @@
-WHILE EXISTS (SELECT child.[Id] FROM [Node] child
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT *
+  FROM	[Node]
+
+
+  WHILE EXISTS (SELECT child.[Id] FROM [Node] child
 				LEFT JOIN [Node] parent ON parent.[Id] = child.Parent_Id
 				WHERE (parent.L IS NOT NULL AND child.L IS NULL) OR (child.Parent_Id IS NULL AND child.L IS NULL))
 BEGIN
@@ -8,17 +13,24 @@ BEGIN
 	SELECT child.Id FROM [Node] child
 	LEFT JOIN [Node] parent ON parent.[Id] = child.Parent_Id
 	WHERE (parent.L IS NOT NULL AND child.L IS NULL) OR child.Parent_Id IS NULL  
-
 	OPEN db_cursor
 
 	FETCH NEXT FROM db_cursor INTO @Id
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
-		DECLARE  @parentLeft INT = (SELECT L from [Node] WHERE [Id] = (SELECT Parent_Id FROM [Node] WHERE [Id] = @Id))
-		DECLARE  @parentRight INT = (SELECT R from [Node] WHERE [Id] = (SELECT Parent_Id FROM [Node] WHERE [Id] = @Id))
-		UPDATE [Node] SET R = R + 2 WHERE R > @parentLeft AND [Id] != @Id
-		UPDATE [Node] SET L = L + 2 WHERE L > @parentLeft AND [Id] != @Id
-		UPDATE [Node] SET L = ISNULL(@parentLeft,0) + 1, R = ISNULL(@parentLeft,0) + 2 WHERE [Id] = @Id
+		IF (SELECT Parent_Id FROM [Node] WHERE [Id] = @Id) IS NOT NULL
+		BEGIN
+			DECLARE @pR INT = (SELECT R FROM [Node] WHERE [Id] = (SELECT Parent_Id FROM [Node] WHERE [Id] = @Id))
+			UPDATE [Node] SET L = L + 2 WHERE L > @pR
+			UPDATE [Node] SET R = R + 2 WHERE R >= @pR
+			UPDATE [Node] SET L = @pR, R = @pr+1 WHERE Id = @Id
+		END 
+		ELSE 
+		BEGIN
+			DECLARE @maxR INT = (SELECT ISNULL(Max(R),0) FROM [Node] WHERE Parent_Id IS NULL)
+			UPDATE [Node] SET L = @maxR+1, R = @maxR+2 WHERE Id = @Id
+		END
+
 		FETCH NEXT FROM db_cursor INTO @Id
 	END
 	CLOSE db_cursor
